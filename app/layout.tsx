62551f96eefbaa8ddb7af616af15e09dfa7f4998
oonuma <oonuma@crew-m.com>
2025-04-08 00:53:52 +0900

    vercel1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { Cormorant_Garamond, Noto_Serif_JP } from 'next/font/google';
 import "./globals.css";
 
